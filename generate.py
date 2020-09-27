05e1a6278bf194acc3e866df8bbab274c3d065cc
Daniel Wolff <daniel.ryan.wolff@gmail.com>
2020-09-27 13:33:05 -0700

    removing schemas folder, removing schemas xml related logic from generate.py

--- generate.py
+++ generate.py
@@ -54,38 +54,6 @@ def parse_config(filename):
     schema['dependencies'] = valid_dependencies
   return schemas
 
-
-def parse_schema(name, data) :
-  schema = ET.fromstring(data)
-
-  template_data = schema.find("template")
-  if template_data is None : fail("Schema '%s' does not contain a 'template' element", name)
-  elif template_data.attrib.get("name") is None : fail("Schema '%s' did not specify a template name", name)
-
-  template_name = template_data.attrib.get("name").strip()
-  template_disp = (template_data.attrib.get("display-name") or template_name).strip()
-  template_desc = (template_data.text or template_name).strip()
-  template_pref = (template_data.attrib.get("prefix") or "cpp").strip()
-  template_deps = []
-
-  dependencies = schema.find("dependencies")
-  if dependencies is not None :
-    for dependency in dependencies.iter("dependency") :
-      dependency_name = dependency.attrib.get("template-name")
-      if dependency_name is None : fail("Schema '%s' has a depency with no 'template-name' attribute.", name)
-      if dependency_name in templates : template_deps.append(dependency_name)
-      else : warn("Schema '%s' depends on a template that could not be found: '%s'", name, dependency_name)
-
-  return {
-    "name": template_name,
-    "display-name": template_disp,
-    "description": template_desc,
-    "prefix": template_pref,
-    "dependencies": template_deps,
-    "raw_schema": schema
-  }
-
-
 def topological_sort(schemas, schema, seen, result) :
   schema_name = schema["name"]
   seen.add(schema_name)
@@ -107,23 +75,14 @@ def resolve_dependencies(schemas, schema) :
 if __name__ == "__main__" :
   parser = argparse.ArgumentParser()
   parser.add_argument('--templates_dir', '-td', type=str, default='templates', help='path to the templates directory')
-  parser.add_argument('--schemas_dir', '-sd', type=str, default='schemas', help='path to the schemas directory')
-  parser.add_argument('--config_file', '-cf', type=str, help='path to the configuration file (this overrides schemas_dir)')
+  parser.add_argument('--config_file', '-cf', type=str, default='config.yaml', help='path to the configuration file (this overrides schemas_dir)')
   parser.add_argument('--output_dir', '-od', type=str, default='/mnt/c/Users/Daniel/AppData/Roaming/Code/User/snippets', help='path to the output directory')
   parser.add_argument('--output_filename', '-of', type=str, default='cp.code-snippets', help='name of the output snippets file')
   args = parser.parse_args()
 
   templates = get_files_in_dir(args.templates_dir)
-  schemas = {}
+  schemas = parse_config(args.config_file)
   out_json = {}
-
-  if args.config_file:
-    schemas = parse_config(args.config_file)
-  else:
-    raw_schemas = get_files_in_dir(args.schemas_dir)
-    for schema_name, data in raw_schemas.items() :
-      schema = parse_schema(schema_name, data)
-      schemas[schema["name"]] = schema
 
   for template_name, template_data in templates.items() :
     schema = schemas[template_name]
